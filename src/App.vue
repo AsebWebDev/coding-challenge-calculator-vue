e2b74ed76bca035185712332978516f6682bd1ec
André Sebastian <a.sebastian@posteo.de>
2019-03-18 15:58:31 +0100

    add calculator component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Calculator</h1>
+    <Calculator msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
@@ -19,8 +19,8 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/Calculator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="calculator">
+    <Display v-bind:display="display"/>
+    <input type="text" v-model="display">
+    <Keypad/>
+  </div>
+</template>
+
+<script>
+// import Display from './Display.vue'
+// import Keypad from './Keypad.vue'
+
+export default {
+  name: 'calculator',
+  components: {
+    // Display, Keypad
+  }, 
+  data () {
+    return {
+      display: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .calculator {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+</style>
